a1ee3e3df10c70ed616924d4ebf7cc0e7644a2bb
ddennis <dennis@fantastisk.dk>
2020-01-21 18:06:23 +0100

    refactor: abstracted each view into it's own components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,76 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import SearchBox from './components/SearchBox';
-import FetchData, {stateProps} from './components/FetchData';
+import FetchData, {FetchResponse} from './components/FetchData';
+import UserListItem from "./components/UserListItem";
+import enviroment from "./enviroment";
+
+import DetailsView from "./components/DetailsView";
+import UserList from './components/UserList';
+import { useSpring, animated } from 'react-spring';
 
 const App: React.FC = () => {
 
+	const [query, setQuery] = useState( "ddennis" );
+	const [userId, setUserId] = useState( "" );
 
-  const [query, setQuery] = useState("")
+	const selectUser = (login: string) => {
 
-  /*const submit = (q:string) => {
-    setQuery(q)
-  }*/
+        set({
+            to: [{display: "block" }, {opacity: 1, y:0}],
+            //from: {opacity: 0, color: 'blue'}
+        })
+
+		setUserId(login)
+
+	};
 
 
-  return (
+    const [props, set, stop] = useSpring(() => ({y:0, display: "none", opacity: 0, config: {friction: 40, tension: 600}}))
 
-    <div className="container-fluid h-100" style={{background:"red"}}>
-      <div className="row h-100" style={{}}>
-      	<div className="col">
+	const closeDetails = () => {
+        set({
+            to: [{opacity: 0, y:0 }, { display: "none"}],
+			onRest: (k) => {
+            	if(k.display === "none" ){
+					setUserId("")
+            	}
+				console.log (" App > end = " , k);
+			}
+        })
 
-          <SearchBox submit={setQuery}></SearchBox>
+    };
 
-          <FetchData query={query}>
-            {
-              (fetchState:stateProps) => {
 
-                console.log (" App > fetchState.data.items = " , fetchState.data.items);
+	return (
 
-                return (
-                    <div>
-                      {
-                        fetchState.data.items.map( (item) => {
-                          return <div key={item.id}>{item.login}</div>
+	    <>
 
-                        })
-                      }
+         <animated.div className="position-relative" style={{zIndex: 500, ...props}}>
+             <DetailsView userId={userId} closeFunc={closeDetails}></DetailsView>
+         </animated.div>
+
+
+
+		<div className="container-fluid vh-100 " style={{overflow:userId === "" ? "auto" : "hidden"}} >
+
+			<div className="row h-100" style={{}}>
+
+				<div className="col-12 h-30 ">
+                    <div className="d-flex h-100 justify-content-center align-items-center">
+                        <SearchBox submit={setQuery}></SearchBox>
                     </div>
-                )
-              }
-            }
-          </FetchData>
+				</div>
 
 
-      	</div>
-      </div>
-    </div>
+				<div className="col-12 col-md-8 mx-auto  ">
+                    <UserList selectUser={selectUser} query={query} ></UserList>
+				</div>
+			</div>
 
-  );
+		</div>
+        </>
+
+	);
 };
 
 export default App;

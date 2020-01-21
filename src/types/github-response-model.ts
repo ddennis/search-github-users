aed572a6a2c5909b32c9b5e4d44ac1d450275468
ddennis <dennis@fantastisk.dk>
2020-01-21 23:03:34 +0100

    chore: added type for github profile response

--- src/types/github-response-model.ts
+++ src/types/github-response-model.ts
@@ -2,10 +2,10 @@
 export interface githubResponse {
 	total_count: number;
 	incomplete_results: boolean;
-	items:Array<userItems>
+	items:Array<GithubUser>
 }
 
-export interface userItems {
+export interface GithubUser {
 	login: string
 	id: number
 	node_id: string
@@ -27,3 +27,36 @@ export interface userItems {
 	score: number
 }
 
+export interface GithubProfile {
+	login: string;
+	id: number;
+	node_id: string;
+	avatar_url: string;
+	gravatar_id: string;
+	url: string;
+	html_url: string;
+	followers_url: string;
+	following_url: string;
+	gists_url: string;
+	starred_url: string;
+	subscriptions_url: string;
+	organizations_url:string;
+	repos_url: string;
+	events_url: string;
+	received_events_url: string;
+	type:  string;
+	site_admin:  string;
+	name:  string;
+	company:  string;
+	blog:  string;
+	location: string;
+	email:  string | null;
+	hireable: boolean,
+	bio:  string | null,
+	public_repos: number,
+	public_gists: number,
+	followers: number,
+	following: number,
+	created_at:  string;
+	updated_at:  string;
+}

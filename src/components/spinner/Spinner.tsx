bd2d7f5e46eaa251b10a8ca00c970c327a38e370
ddennis <dennis@fantastisk.dk>
2020-01-22 02:58:09 +0100

    fix: error message did not show

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,42 +1,39 @@
-import React, { useCallback , useState} from "react";
-import { useSpring, animated } from 'react-spring';
+import React from "react";
+
 import './spinner.scss'
 
-type Props ={
-	error:string | null;
-
+type Props = {
+	error: string | null;
 }
-
 
 const Spinner: React.FC<Props> = (props) => {
 
 	const {error} = props;
 
 	console.log (" Spinner > error = " , error);
-	const [isActive, setActive] = useState(true);
+
 	const statusTxt = !error ? "Getting energy data..." : error;
 
+	return (
+		<div className="row">
+			<div className="col-12  text-center">
 
-  return (
-	  <div className="row" 	 >
-		<div className="col-12 text-center" >
+				{!error ?
 
-		  {!error ?
+					<div className="loader"></div>
 
-			  <div className="loader"></div>
+					:
 
-			  :
-
-			  <div style={{padding:20}}>
-				  <h1>adasd</h1>
-				  <p>An error happend:</p>
-				  <p>{statusTxt}</p>
-
-			  </div>
-		  }
+					<div className="w-100 d-flex flex-column justify-content-center align-items-center " style={{padding: 20}}>
+						<div className="w-50">
+							<h1>An error happend:</h1>
+							<p>{statusTxt}</p>
+						</div>
+					</div>
+				}
+			</div>
 		</div>
-	  </div>
-  );
+	);
 };
 
 export default Spinner
